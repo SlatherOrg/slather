ecb3e9a7d8d279ffe0760927040d66f123b3abc6
Mark Larsen <larse503@gmail.com>
2014-06-20 15:00:13 -0700

    Setup a project for coverage using slather setup. Make sure there are coverage files, and if not, raise an error.

--- lib/slather/project.rb
+++ lib/slather/project.rb
@@ -25,35 +25,24 @@ module Slather
     private :build_directory
 
     def coverage_files
-      Dir["#{build_directory}/**/*.gcno"].map do |file|
+      coverage_files = Dir["#{build_directory}/**/*.gcno"].map do |file|
         coverage_file = coverage_file_class.new(file)
         coverage_file.project = self
         # If there's no source file for this gcno, it probably belongs to another project.
-        puts regex
         if coverage_file.source_file_pathname && !(coverage_file.source_file_pathname_relative_to_project_root.to_s =~ /^(#{ignore_list.join("|")})$/)
           coverage_file
         else
           nil
         end
       end.compact
+
+      if coverage_files.empty?
+        raise StandardError, "No coverage files found. Are you sure your project is setup for generating coverage files? Try `slather setup your/project.pbxproj`"
+      else
+        coverage_files
+      end
     end
     private :coverage_files
-
-    def coveralls_coverage_data
-      {
-        :service_job_id => ENV['TRAVIS_JOB_ID'],
-        :service_name => "travis-ci",
-        :source_files => coverage_files.map(&:as_json)
-      }.to_json
-    end
-    private :coveralls_coverage_data
-
-    def post_to_coveralls
-      f = File.open('coveralls_json_file', 'w+')
-      f.write(coveralls_coverage_data)
-      `curl -s --form json_file=@#{f.path} https://coveralls.io/api/v1/jobs`
-      FileUtils.rm(f)
-    end
 
     def self.yml_file
       @yml_file ||= begin
@@ -77,6 +66,13 @@ module Slather
         extend(Slather::CoverageService::SimpleOutput)
       elsif !self.class.method_defined?(:post)
         raise ArgumentError, "value `#{coverage_service}` not valid for key `coverage_service` in #{self.class.yml_file.path}. Try `terminal` or `coveralls`"
+      end
+    end
+
+    def setup_for_coverage
+      build_configurations.each do |build_configuration|
+        build_configuration.build_settings["GCC_INSTRUMENT_PROGRAM_FLOW_ARCS"] = "YES"
+        build_configuration.build_settings["GCC_GENERATE_TEST_COVERAGE_FILES"] = "YES"
       end
     end
 
