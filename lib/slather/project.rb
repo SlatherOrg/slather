45993392bc70f62d149994a7ecaac7e2979f2dcf
Mark Larsen <larse503@gmail.com>
2014-06-20 13:13:10 -0700

    get configuration from yml

--- lib/slather/project.rb
+++ lib/slather/project.rb
@@ -1,11 +1,18 @@
 require 'fileutils'
 require 'xcodeproj'
 require 'json'
+require 'yaml'
 
 module Slather
   class Project < Xcodeproj::Project
 
     attr_accessor :build_directory, :ignore_list
+
+    def self.open(xcodeproj)
+      proj = super
+      proj.configure_from_yml
+      proj
+    end
 
     def derived_data_dir
       File.expand_path('~') + "/Library/Developer/Xcode/DerivedData/"
@@ -22,6 +29,7 @@ module Slather
         coverage_file = coverage_file_class.new(file)
         coverage_file.project = self
         # If there's no source file for this gcno, it probably belongs to another project.
+        puts regex
         if coverage_file.source_file_pathname && !(coverage_file.source_file_pathname_relative_to_project_root.to_s =~ /^(#{ignore_list.join("|")})$/)
           coverage_file
         else
@@ -45,6 +53,31 @@ module Slather
       f.write(coveralls_coverage_data)
       `curl -s --form json_file=@#{f.path} https://coveralls.io/api/v1/jobs`
       FileUtils.rm(f)
+    end
+
+    def self.yml_file
+      @yml_file ||= begin
+        yml_filename = '.slather.yml'
+        if File.exist?(yml_filename)
+          YAML.load_file(yml_filename)
+        else
+          {}
+        end
+      end
+    end
+
+    def configure_from_yml
+      self.build_directory = self.class.yml_file["build_directory"] if self.class.yml_file["build_directory"]
+      self.ignore_list = self.class.yml_file["ignore"] || []
+
+      coverage_service = self.class.yml_file["coverage_service"]
+      if coverage_service == "coveralls"
+        extend(Slather::CoverageService::Coveralls)
+      elsif coverage_service == "terminal"
+        extend(Slather::CoverageService::SimpleOutput)
+      elsif !self.class.method_defined?(:post)
+        raise ArgumentError, "value `#{coverage_service}` not valid for key `coverage_service` in #{self.class.yml_file.path}. Try `terminal` or `coveralls`"
+      end
     end
 
   end
