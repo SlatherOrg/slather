c520a99ee5609a4393b3a5d41775ce149e9ba958
Kyle Fuller <inbox@kylefuller.co.uk>
2014-09-17 12:56:40 +0200

    Implement a CocoaPods plugin

---
+++ lib/cocoapods_plugin.rb
@@ -0,0 +1,10 @@
+require 'slather'
+
+Pod::HooksManager.register(:post_install) do |installer_context|
+  sandbox_root = installer_context.sandbox_root
+  sandbox = Pod::Sandbox.new(sandbox_root)
+  project = Xcodeproj::Project.open(sandbox.project_path)
+  project.slather_setup_for_coverage()
+  project.save()
+end
+
