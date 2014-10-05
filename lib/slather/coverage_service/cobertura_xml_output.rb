6e1ab7680e9d03e868aab596f5bfa84dd90ced5f
Julian Krumow <julian.krumow@tarbrain.com>
2014-10-05 17:30:27 +0200

    Add basic setup of cobertura report file.

--- lib/slather/coverage_service/cobertura_xml_output.rb
+++ lib/slather/coverage_service/cobertura_xml_output.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Slather
   module CoverageService
     module CoberturaXmlOutput
@@ -9,7 +11,37 @@ module Slather
 
       def post
         output = { 'meta' => { 'timestamp' => DateTime.now.strftime('%Y-%m-%d %H:%M:%S.%6N') } }
-        symbols = {}
+
+        @doc = Nokogiri::XML "<coverage></coverage>"
+        @doc.to_xml
+        
+        coverage = @doc.at_css "coverage"
+        coverage['line-rate'] = '20.0'
+        coverage['branch-rate'] = "0.0" 
+        coverage['version'] = "1.9"
+        coverage['timestamp'] = "1187353747005"
+
+        sources  = Nokogiri::XML::Node.new "sources", @doc
+        packages = Nokogiri::XML::Node.new "packages", @doc
+        classes  = Nokogiri::XML::Node.new "classes", @doc
+        methods  = Nokogiri::XML::Node.new "methods", @doc
+        lines    = Nokogiri::XML::Node.new "lines", @doc
+
+        sources.parent = coverage
+        packages.parent = coverage
+        classes.parent = coverage
+        methods.parent = coverage
+        lines.parent = coverage
+
+        # <sources>
+        #   <source>C:/local/mvn-coverage-example/src/main/java</source>
+        # </sources>
+        
+        @doc.to_xml
+
+        puts @doc.to_xml
+
+
 
         coverage_files.each do |coverage_file|
           next unless coverage_file.gcov_data
@@ -19,13 +51,10 @@ module Slather
 
           coverage_file.gcov_data.split("\n").each do |line|
             
-            # assemble XML
-
 
           end
         end
 
-        output['symbols_by_file'] = symbols
         File.open('cobertura.xml', 'w') { |file| file.write(output.to_s) }
       end
 
