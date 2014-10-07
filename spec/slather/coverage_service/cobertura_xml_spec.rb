8c7c2be5f0a1d8a51dc29c23c0e5f77ca5193d79
Julian Krumow <julian.krumow@tarbrain.com>
2014-10-07 22:26:41 +0200

    Enhance cobertura_xml_spec.

--- spec/slather/coverage_service/cobertura_xml_spec.rb
+++ spec/slather/coverage_service/cobertura_xml_spec.rb
@@ -14,6 +14,35 @@ describe Slather::CoverageService::CoberturaXmlOutput do
     end
   end
 
+  describe '#create_xml_report' do
+    
+    it "should create a coverage-node which is the root node of the XML document" do
+    end
+    it "should create timestamp, the overall line-rate and branch-rate attributes inside the coverage-node" do
+    end
+
+    it "should create a package-node" do
+    end
+    it "should create name, line-rate and branch-rate attributes inside the package-node" do
+    end
+
+    it "should create a class-node for each coverage file" do
+    end
+    it "should create name, filename, line-rate, branch-rate and complexity attributes inside the class-node" do
+    end
+
+    it "should create a method-node for each testable method inside the coverage file" do
+    end
+    it "should create name, line-rate, branch-rate and signature attributes inside the method-node" do
+    end
+
+    it "should create a line-node for each line inside a testable method" do
+    end
+    it "should create number, hits, and branch attributes inside the line-node" do
+    end
+
+  end
+
   describe '#post' do
     it "should print out the coverage for each file, and then total coverage" do
       fixtures_project.post
