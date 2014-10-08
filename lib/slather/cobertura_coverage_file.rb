b9fd7d4ecfe88fc9f5a23e6f07e7f0385c36ffd1
Julian Krumow <julian.krumow@tarbrain.com>
2014-10-09 01:37:53 +0200

    Add branch coverage.
    Needs refactoring.

--- lib/slather/cobertura_coverage_file.rb
+++ lib/slather/cobertura_coverage_file.rb
@@ -1,8 +1,44 @@
 module Slather
   class CoberturaCoverageFile < CoverageFile
 
+    def gcov_data
+      @gcov_data ||= begin
+        gcov_output = `gcov "#{source_file_pathname}" --object-directory "#{gcno_file_pathname.parent}" --branch-probabilities`
+        # Sometimes gcov makes gcov files for Cocoa Touch classes, like NSRange. Ignore and delete later.
+        gcov_files_created = gcov_output.scan(/creating '(.+\..+\.gcov)'/)
+
+        gcov_file_name = "./#{source_file_pathname.basename}.gcov"
+        if File.exists?(gcov_file_name)
+          gcov_data = File.new(gcov_file_name).read
+        end
+
+        gcov_files_created.each { |file| FileUtils.rm_f(file) }
+
+        gcov_data
+      end
+    end
+
+    def coverage_for_line(line)
+      line =~ /^(.+?):/
+
+      if line === nil || $1 === nil
+        return nil
+      end
+
+      match = $1.strip
+      case match
+      when /[0-9]+/
+        match.to_i
+      when /#+/
+        0
+      when "-"
+        nil
+      end
+    end
+
     def cleaned_gcov_data
-      return gcov_data.gsub(/^.*?:.*?:\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*\/(.)*\s/, '')
+      cleaned_data = gcov_data.gsub(/^.*?:.*?:\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*\/(.)*\s/, '')
+      return cleaned_data.gsub(/^function(.*) called [0-9]+ returned [0-9]+% blocks executed(.*)$/, '')
     end
 
     def lines_grouped_by_methods
@@ -12,6 +48,8 @@ module Slather
 
       # remove lines that are commented out
       cleaned_gcov_data.split("\n").each do |line|
+
+        # puts line
 
         # extract code after second colon
         line_of_code = line.sub(/.*?:.*?:/, '')
@@ -83,7 +121,7 @@ module Slather
       classNode['name'] = filename
       classNode['filename'] = filepath
       classNode['line-rate'] = '%.2f' % [rate_lines_tested]
-      classNode['branch-rate'] = '0.0'
+      classNode['branch-rate'] = '0.0' # TODO: calculate branch rate
       classNode['complexity'] = '1.0'
 
       methodsNode = Nokogiri::XML::Node.new "methods", xml_document
@@ -99,7 +137,7 @@ module Slather
     def create_method_node(method, xml_document)
       methodNode = Nokogiri::XML::Node.new "method", xml_document
       methodNode['name'] = method["name"]
-      methodNode['branch-rate'] = 0.0
+      methodNode['branch-rate'] = '1.0'
       methodNode['signature'] = "()V" # TODO: parse method signature ? Actually not necessary in obj-c.
 
       linesNode = Nokogiri::XML::Node.new "lines", xml_document
@@ -107,30 +145,76 @@ module Slather
 
       method_lines = 0
       method_lines_tested = 0
+      branch_rates = 0
+      branches_tested = 0
+
+      line_node = nil
+      branches = Array.new
       method["lines_of_code"].each do |line|
+
         line_segments = line.split(':')
 
         # skip all lines which are not relevant
-        if line_segments[0].strip === '-'
+        if line_segments.length === 0 || line_segments[0].strip === '-'
           next
         end
 
-        lineNode = Nokogiri::XML::Node.new "line", xml_document
-        lineNode.parent = linesNode
-        lineNode['number'] = line_segments[1].strip
-        hits = coverage_for_line(line)
-        lineNode['hits'] = hits
-        lineNode['branch'] = "false"
+        # process lines with branch information
+        if line_segments[0].match(/^branch(.*)/)
+          line_node['branch'] = "true"
+          branches.push(branch_coverage_for_line(line))
+        else
+          # process collected branch data from previous line
+          if !branches.empty?
+            conditions_node = Nokogiri::XML::Node.new "conditions", xml_document
+            conditions_node.parent = line_node
+            condition_node = Nokogiri::XML::Node.new "condition", xml_document
+            condition_node.parent = conditions_node
+            condition_node['number'] = "0"
+            condition_node['type'] = "jump"
 
-        method_lines += 1
-        if hits > 0
-          method_lines_tested += 1
+            condition_coverage = 0
+            branch_hits = 0
+            branches.each do |branch|
+              condition_coverage += branch
+              if branch > 0
+                branch_hits += 1
+              end
+            end
+            condition_coverage = (branches.inject(:+) / branches.length)
+            condition_node['coverage'] = "#{condition_coverage}%"
+            line_node['condition-coverage'] = "#{condition_coverage}% (#{branch_hits}/#{branches.length})"
+            branches_tested += 1
+            branch_rates += condition_coverage
+            branches.clear
+          end
+
+          line_node = Nokogiri::XML::Node.new "line", xml_document
+          line_node.parent = linesNode
+          line_node['number'] = line_segments[1].strip
+          hits = coverage_for_line(line)
+          line_node['hits'] = hits
+          line_node['branch'] = "false"
+
+          method_lines += 1
+          if hits > 0
+            method_lines_tested += 1
+          end
+
         end
       end
 
       total_method_line_rate = '%.2f' % (method_lines_tested / method_lines.to_f)
       methodNode['line-rate'] = total_method_line_rate
+      if branches_tested > 0
+        total_method_branch_rate = '%.2f' % [(branch_rates / branches_tested.to_f) / 100.0]
+        methodNode['branch-rate'] = total_method_branch_rate
+      end
       return methodNode
+    end
+
+    def branch_coverage_for_line(block)
+      return block.split(' ')[3].strip.gsub(/%/, '').to_i
     end
 
   end
