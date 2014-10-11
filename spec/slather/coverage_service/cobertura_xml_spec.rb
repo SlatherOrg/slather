7ae25ea49275413e4e9dd08d8dd7fc5914ecc690
Julian Krumow <julian.krumow@tarbrain.com>
2014-10-12 01:42:16 +0200

    Update spec for cobertura xml.

--- spec/slather/coverage_service/cobertura_xml_spec.rb
+++ spec/slather/coverage_service/cobertura_xml_spec.rb
@@ -14,46 +14,23 @@ describe Slather::CoverageService::CoberturaXmlOutput do
     end
   end
 
-  describe '#create_xml_report' do
-    
-    it "should create a coverage-node which is the root node of the XML document" do
-    end
-    it "should create timestamp, the overall line-rate and branch-rate attributes inside the coverage-node" do
-    end
-
-    it "should create a package-node" do
-    end
-    it "should create name, line-rate and branch-rate attributes inside the package-node" do
-    end
-
-    it "should create a class-node for each coverage file" do
-    end
-    it "should create name, filename, line-rate, branch-rate and complexity attributes inside the class-node" do
-    end
-
-    it "should create a method-node for each testable method inside the coverage file" do
-    end
-    it "should create name, line-rate, branch-rate and signature attributes inside the method-node" do
-    end
-
-    it "should create a line-node for each line inside a testable method" do
-    end
-    it "should create number, hits, and branch attributes inside the line-node" do
-    end
-
-  end
-
   describe '#post' do
-    it "should print out the coverage for each file, and then total coverage" do
+    it "should create an XML report spanning all coverage files" do
       fixtures_project.post
+      
+      file = File.open(FIXTURES_XML_PATH)
+      fixture_xml_doc = Nokogiri::XML(file)
+      file.close
+      fixture_xml_doc.root['timestamp'] = ''
+      fixture_xml_doc.root['version'] = ''
 
-      # fixture_json = JSON.parse(File.read(FIXTURES_JSON_PATH))
-      # fixture_json['meta']['timestamp'] = ''
+      file = File.open('cobertura.xml')
+      current_xml_doc = Nokogiri::XML(file)
+      file.close
+      current_xml_doc.root['timestamp'] = ''
+      current_xml_doc.root['version'] = ''
 
-      # current_json = JSON.parse(File.read('.gutter.json'))
-      # current_json['meta']['timestamp'] = ''
-
-      # expect(current_json).to eq(fixture_json)
+      expect(current_xml_doc.to_xml).to eq(fixture_xml_doc.to_xml)
       
       File.unlink('cobertura.xml')
     end
