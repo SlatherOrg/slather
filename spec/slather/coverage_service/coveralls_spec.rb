4d074781fc42fbd15b38bf9457a6a8b39a8338a4
Richard Lee <dlackty@gmail.com>
2014-11-07 14:20:36 +0800

    Revert Revert "Add support for ci_access_token and :travis_pro"
    This reverts commit 3922cd17fee101ee9804f3bc483d4b67bf22c80b.

--- spec/slather/coverage_service/coveralls_spec.rb
+++ spec/slather/coverage_service/coveralls_spec.rb
@@ -36,6 +36,21 @@ describe Slather::CoverageService::Coveralls do
       end
     end
 
+    context "coverage_service is :travis_pro" do
+      before(:each) { fixtures_project.ci_service = :travis_pro }
+
+      it "should return valid json for coveralls coverage data" do
+        fixtures_project.stub(:travis_job_id).and_return("9182")
+        fixtures_project.stub(:ci_access_token).and_return("abc123")
+        expect(fixtures_project.send(:coveralls_coverage_data)).to eq("{\"service_job_id\":\"9182\",\"service_name\":\"travis-pro\",\"repo_token\":\"abc123\",\"source_files\":[{\"name\":\"spec/fixtures/fixtures/fixtures.m\",\"source\":\"//\\n//  fixtures.m\\n//  fixtures\\n//\\n//  Created by Mark Larsen on 6/24/14.\\n//  Copyright (c) 2014 marklarr. All rights reserved.\\n//\\n\\n#import \\\"fixtures.h\\\"\\n\\n@implementation fixtures\\n\\n- (void)testedMethod\\n{\\n    NSLog(@\\\"tested\\\");\\n}\\n\\n- (void)untestedMethod\\n{\\n    NSLog(@\\\"untested\\\");\\n}\\n\\n@end\\n\",\"coverage\":[null,null,null,null,null,null,null,null,null,null,null,null,null,null,1,1,null,null,null,0,0,null,null]},{\"name\":\"spec/fixtures/fixtures/more_files/peekaview.m\",\"source\":\"//\\n//  peekaview.m\\n//  fixtures\\n//\\n//  Created by Mark Larsen on 6/25/14.\\n//  Copyright (c) 2014 marklarr. All rights reserved.\\n//\\n\\n#import \\\"peekaview.h\\\"\\n\\n@implementation peekaview\\n\\n- (id)initWithFrame:(CGRect)frame\\n{\\n    self = [super initWithFrame:frame];\\n    if (self) {\\n        // Initialization code\\n    }\\n    return self;\\n}\\n\\n/*\\n// Only override drawRect: if you perform custom drawing.\\n// An empty implementation adversely affects performance during animation.\\n- (void)drawRect:(CGRect)rect\\n{\\n    // Drawing code\\n}\\n*/\\n\\n@end\\n\",\"coverage\":[null,null,null,null,null,null,null,null,null,null,null,null,0,null,0,0,null,0,0,0,null,null,null,null,null,null,null,null,null,null,null]},{\"name\":\"spec/fixtures/fixturesTests/fixturesTests.m\",\"source\":\"//\\n//  fixturesTests.m\\n//  fixturesTests\\n//\\n//  Created by Mark Larsen on 6/24/14.\\n//  Copyright (c) 2014 marklarr. All rights reserved.\\n//\\n\\n#import <XCTest/XCTest.h>\\n#import \\\"fixtures.h\\\"\\n\\n@interface fixturesTests : XCTestCase\\n\\n@end\\n\\n@implementation fixturesTests\\n\\n- (void)setUp\\n{\\n    [super setUp];\\n    // Put setup code here. This method is called before the invocation of each test method in the class.\\n}\\n\\n- (void)tearDown\\n{\\n    // Put teardown code here. This method is called after the invocation of each test method in the class.\\n    [super tearDown];\\n}\\n\\n- (void)testExample\\n{\\n    fixtures *f = [[fixtures alloc] init];\\n    [f testedMethod];\\n}\\n\\n@end\\n\",\"coverage\":[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,1,null,1,null,null,null,null,1,1,null,null,null,1,1,1,null,null]},{\"name\":\"spec/fixtures/fixturesTests/peekaviewTests.m\",\"source\":\"//\\n//  peekaviewTests.m\\n//  fixtures\\n//\\n//  Created by Mark Larsen on 6/25/14.\\n//  Copyright (c) 2014 marklarr. All rights reserved.\\n//\\n\\n#import <XCTest/XCTest.h>\\n\\n@interface peekaviewTests : XCTestCase\\n\\n@end\\n\\n@implementation peekaviewTests\\n\\n- (void)setUp\\n{\\n    [super setUp];\\n    // Put setup code here. This method is called before the invocation of each test method in the class.\\n}\\n\\n- (void)tearDown\\n{\\n    // Put teardown code here. This method is called after the invocation of each test method in the class.\\n    [super tearDown];\\n}\\n\\n- (void)testExample\\n{\\n    XCTAssert(YES, @\\\"woot\\\");\\n}\\n\\n@end\\n\",\"coverage\":[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,1,null,1,null,null,null,null,1,1,null,null,null,2,1,null,null]}]}")
+      end
+
+      it "should raise an error if there is no TRAVIS_JOB_ID" do
+        fixtures_project.stub(:travis_job_id).and_return(nil)
+        expect { fixtures_project.send(:coveralls_coverage_data) }.to raise_error(StandardError)
+      end
+    end
+
     it "should raise an error if it does not recognize the ci_service" do
       fixtures_project.ci_service = :jenkins_ci
       expect { fixtures_project.send(:coveralls_coverage_data) }.to raise_error(StandardError)
