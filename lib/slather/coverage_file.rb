70c5d05918274cd267c6d1755a1d726a6fc9e6bc
Ben <me@benng.me>
2015-03-31 02:19:29 -0500

    Add Obj-C++, precompiled headers, C++, and Swift

--- lib/slather/coverage_file.rb
+++ lib/slather/coverage_file.rb
@@ -14,10 +14,13 @@ module Slather
         # TODO: Handle Swift
         path = nil
         if project.source_directory
-          path = Dir["#{project.source_directory}/**/#{base_filename}.m"].first
+          path = Dir["#{project.source_directory}/**/#{base_filename}.{m,mm,cpp,hpp,swift}"].first
           path &&= Pathname(path)
         else
-          pbx_file = project.files.detect { |pbx_file| pbx_file.real_path.basename.to_s == "#{base_filename}.m" }
+          pbx_file = project.files.detect { |pbx_file|
+            t = Regexp.new(Regexp.escape(base_filename.to_s) + "\.(m|mm|cpp|hpp|swift)$")
+            !t.match(pbx_file.real_path.basename.to_s).nil?
+          }
           path = pbx_file && pbx_file.real_path
         end
         path
@@ -112,7 +115,7 @@ module Slather
     def branch_coverage_data
       @branch_coverage_data ||= begin
         branch_coverage_data = Hash.new
-        
+
           gcov_data.scan(/(^(\s+(-|#+|[0-9]+):\s+[1-9]+:(.*)$\r?\n)(^branch\s+[0-9]+\s+[a-zA-Z0-9]+\s+[a-zA-Z0-9]+$\r?\n)+)+/) do |data|
             lines = data[0].split("\n")
             line_number = lines[0].split(':')[1].strip.to_i
