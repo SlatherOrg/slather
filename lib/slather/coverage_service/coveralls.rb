225bbf71bc0c14568814727ade37c2f5ae4b2668
David Ohayon <david@evertrue.com>
2015-05-07 14:39:13 -0400

    puts json

--- lib/slather/coverage_service/coveralls.rb
+++ lib/slather/coverage_service/coveralls.rb
@@ -93,13 +93,15 @@ module Slather
           end
         elsif ci_service == :jenkins
           if jenkins_job_id
-            {
+            json = {
               service_job_id: jenkins_job_id,
               service_name: "jenkins",
               repo_token: ci_access_token,
               source_files: coverage_files.map(&:as_json),
               git: jenkins_git_info
             }.to_json
+            puts json
+            json
           else
             raise StandardError, "Environment variable `BUILD_ID` not set. Is this running on a jenkins build?"
           end
