6c659e41b885236d4ba41f699629ff41ef1eaf21
Michael Myers <michael.morris.myers@gmail.com>
2016-02-15 12:00:01 -0500

    added build statistic reporting for teamcity
    with addition to spec

--- lib/slather/coverage_service/simple_output.rb
+++ lib/slather/coverage_service/simple_output.rb
@@ -26,6 +26,22 @@ module Slather
 
           puts "#{coverage_file.source_file_pathname_relative_to_repo_root}: #{lines_tested} of #{total_lines} lines (#{percentage}%)"
         end
+
+        # check if there needs to be custom reporting based on the ci service
+        if ci_service == :teamcity
+          # TeamCity Build Statistic Reporting
+          #
+          # Reporting format ##teamcity[buildStatisticValue key='<valueTypeKey>' value='<value>']
+          # key='CodeCoverageAbsLCovered' is total number of lines covered
+          # key='CodeCoverageAbsLTotal' is total number of lines
+          #
+          # Sources:
+          # - https://confluence.jetbrains.com/display/TCDL/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-ReportingBuildStatistics
+          # - https://confluence.jetbrains.com/display/TCDL/Custom+Chart#CustomChart-listOfDefaultStatisticValues
+          puts "##teamcity[buildStatisticValue key='CodeCoverageAbsLCovered' value='%i']" % total_project_lines_tested
+          puts "##teamcity[buildStatisticValue key='CodeCoverageAbsLTotal' value='%i']" % total_project_lines
+        end
+
         total_percentage = '%.2f' % [(total_project_lines_tested / total_project_lines.to_f) * 100.0]
         puts "Test Coverage: #{total_percentage}%"
       end
