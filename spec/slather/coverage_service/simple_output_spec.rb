ca811fe08a21368e0d2114a4786a547d67fcd648
Kent Sutherland <git@ksuther.com>
2016-04-17 17:14:01 -0400

    Added fixturesTwo dynamic library to test Xcode project. Updated expected coverage output to pass. Only testing multiple binaries in simple_output_spec.rb to keep things simpler in the other test cases.

--- spec/slather/coverage_service/simple_output_spec.rb
+++ spec/slather/coverage_service/simple_output_spec.rb
@@ -5,6 +5,7 @@ describe Slather::CoverageService::SimpleOutput do
   let(:fixtures_project) do
     proj = Slather::Project.open(FIXTURES_PROJECT_PATH)
     proj.build_directory = TEMP_DERIVED_DATA_PATH
+    proj.binary_basename = ["fixturesTests", "libfixturesTwo"]
     proj.input_format = "profdata"
     proj.configure
     proj
@@ -21,9 +22,10 @@ describe Slather::CoverageService::SimpleOutput do
       ["spec/fixtures/fixtures/fixtures.m: 3 of 6 lines (50.00%)",
       "spec/fixtures/fixtures/more_files/Branches.m: 13 of 30 lines (43.33%)",
       "spec/fixtures/fixturesTests/BranchesTests.m: 16 of 16 lines (100.00%)",
-      "spec/fixtures/fixturesTests/fixturesTests.m: 12 of 12 lines (100.00%)",
+      "spec/fixtures/fixturesTests/fixturesTests.m: 17 of 17 lines (100.00%)",
       "spec/fixtures/fixturesTests/peekaviewTests.m: 11 of 11 lines (100.00%)",
-      "Test Coverage: 73.33%"
+      "spec/fixtures/fixturesTwo/fixturesTwo.m: 6 of 6 lines (100.00%)",
+      "Test Coverage: 76.74%"
       ].each do |line|
         expect(fixtures_project).to receive(:puts).with(line)
       end
@@ -40,11 +42,12 @@ describe Slather::CoverageService::SimpleOutput do
           ["spec/fixtures/fixtures/fixtures.m: 3 of 6 lines (50.00%)",
            "spec/fixtures/fixtures/more_files/Branches.m: 13 of 30 lines (43.33%)",
            "spec/fixtures/fixturesTests/BranchesTests.m: 16 of 16 lines (100.00%)",
-           "spec/fixtures/fixturesTests/fixturesTests.m: 12 of 12 lines (100.00%)",
+           "spec/fixtures/fixturesTests/fixturesTests.m: 17 of 17 lines (100.00%)",
            "spec/fixtures/fixturesTests/peekaviewTests.m: 11 of 11 lines (100.00%)",
-           "##teamcity[buildStatisticValue key='CodeCoverageAbsLCovered' value='55']",
-           "##teamcity[buildStatisticValue key='CodeCoverageAbsLTotal' value='75']",
-           "Test Coverage: 73.33%"
+           "spec/fixtures/fixturesTwo/fixturesTwo.m: 6 of 6 lines (100.00%)",
+           "##teamcity[buildStatisticValue key='CodeCoverageAbsLCovered' value='66']",
+           "##teamcity[buildStatisticValue key='CodeCoverageAbsLTotal' value='86']",
+           "Test Coverage: 76.74%"
           ].each do |line|
             expect(fixtures_project).to receive(:puts).with(line)
           end
